9e92bd00cce816e5219a76683c4d2c1cb0a61bbc
GURUPREET6365 <kumargurupreet2008@gmail.com>
2026-01-04 15:03:44 +0530

    upgraded this system into modern website, added chrome notification.

--- my_attendance/views.py
+++ my_attendance/views.py
@@ -4,7 +4,11 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def home(request):
-    return render(request, 'my_attendance/home.html')
+    context = {}
+    if request.user.is_authenticated:
+        attendance_records = Attendance.objects.filter(user=request.user).order_by('-date')
+        context['attendance_records'] = attendance_records
+    return render(request, 'my_attendance/home.html', context)
 
 def about(request):
     return render(request, 'my_attendance/about.html')
